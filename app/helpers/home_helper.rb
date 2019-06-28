513a728c32e580e6ceb0eeb0a0a4335170f6e89d
Emmanuel Hayford <siawmensah@gmail.com>
2019-06-28 23:48:00 +0200

    Add basic header and methods

---
+++ app/helpers/home_helper.rb
@@ -0,0 +1,39 @@
+module HomeHelper
+  def user_signed_in?
+    session['userinfo'].present?
+  end
+
+  def authenticate_user!
+    if user_signed_in?
+      @current_user = session['userinfo']
+    else
+      redirect_to root_path
+    end
+  end
+
+  def current_user
+    @current_user
+  end
+
+  # def reset_session
+  #   session['userinfo'] = nil if session['userinfo'].present?
+  # end
+
+  def greeting
+    if current_user.present?
+      @greeting = "Welcome, John Doe!"
+      # @link = dashboard_path
+    else
+      @greeting = 'Tweetsy'
+      @link = root_path
+    end
+  end
+
+  def login_or_out
+    if current_user.present?
+      link_to('Log Out', logout_path, class: 'nav-link')
+    else
+      link_to('Log In', root_path, class: 'nav-link')
+    end
+  end
+end
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,4 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  # Regenerate keys later!
+  provider :twitter, "B4vTxb4bkgn2j2hd7sSMYFfaB", "csu0DJq1SROK76lDtyYeuTToM6RfpLcMXnNkbOr1dSUd0Tcokb"
+end
