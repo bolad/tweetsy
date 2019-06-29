be63bf2d0bc2c66a53cd8dda91c19e9b60a9b4f7
Emmanuel Hayford <siawmensah@gmail.com>
2019-06-29 12:57:01 +0200

    Change DB to PG

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_06_29_085214) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "packs", force: :cascade do |t|
     t.string "name"
     t.integer "user_id", null: false
